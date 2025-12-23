37264fb7205007e7a2ee5e65f735ed5492014493
sriramsuryaa <sriramsuryaa@outlook.com>
2025-12-23 18:30:45 +0530

    Modified the tasks order

---
+++ 04.Scripting-Task/Scripting-Task-2/replace.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+content="./content.txt"
+output="./replaced-content.txt"
+
+
+function replace_as_new_file()
+{
+  sed '1,4b; /welcome/ s/give/learning/g' $content > $output
+
+   if [ -f "$output" ]; 
+     then
+       echo "Replaced file : replaced-content.txt"
+     fi
+}
+
+function replace_in_actual_file()
+{
+  sed -i '1,4b; /welcome/ s/give/learning/g' $content
+}
+
+replace_as_new_file
+
