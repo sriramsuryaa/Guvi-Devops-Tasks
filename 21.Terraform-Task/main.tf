d89672b2abde5ff43f24baf64055b6bda855d387
sriramsuryaa <sriramsuryaa@outlook.com>
2026-02-13 15:41:43 +0530

    updated the terraform code in single file

--- 21.Terraform-Task/main.tf
+++ 21.Terraform-Task/main.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.31.0"
+    }
+  }
+}
+
+provider "aws" {
+  alias  = "aps-1"
+  region = "ap-south-1"
+}
+
+provider "aws" {
+  alias  = "aps-2"
+  region = "ap-south-2"
+}
+
 data "aws_ami" "ubuntu_aps1" {
   provider    = aws.aps-1
   most_recent = true
